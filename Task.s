b9c2a2e1c5f0bd5a4cc87c3e1f929ca59971ddba
Milorann <57359954+Milorann@users.noreply.github.com>
2022-10-22 12:55:31 +0300

    Update Task.s

--- Task.s
+++ Task.s
@@ -1,4 +1,3 @@
-	.file	"Task_in_C.c"
 	.intel_syntax noprefix
 	.text
 	.section	.rodata
@@ -195,22 +194,3 @@ main:
 	pop	r15
 	pop	rbp
 	ret
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
