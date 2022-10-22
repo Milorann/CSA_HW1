71c73433e3f3be98893b722364b83df1026ad40c
Milorann <57359954+Milorann@users.noreply.github.com>
2022-10-22 15:50:47 +0300

    Update Task_in_C.c

--- Task_in_C.c
+++ Task_in_C.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+static int A[100];
+static int B[100];
+
 int main()
 {
     int n;
+    printf("Введите разер массива (от 1 до 100 включительно), затем введите элементы массива: ");
     scanf("%d", &n);
-    
-    int A[n];
-    int B[n];
-    
+
+    if(n < 1 || n > 100){
+        printf("Неправильный размер массива. Завершение программы.\n");
+        return 0;
+    }
+
     for(int i = 0; i < n; i++){
         scanf("%d", &A[i]);
         if(A[i] > 0){
@@ -24,5 +30,7 @@ int main()
     for(int i = 0; i<n; i++){
         printf("%d ", B[i]);
     }
+    
+    printf("\n");
     return 0;
 }
